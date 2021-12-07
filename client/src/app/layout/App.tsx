fe6bd9670cad832c3bf7464a3ada49be0f8fe5db
marius.starke <marius.starke@flyeralarm-industrialprint.com>
2021-12-07 11:40:51 +0100

    End of section 4

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,5 +1,5 @@
 import Header from "./Header";
-import Catalog from "../../features/catalog/Catalog";
+import HomePage from "../../features/home/HomePage";
 import {
     Container,
     createTheme,
@@ -7,6 +7,11 @@ import {
     ThemeProvider,
 } from "@mui/material";
 import { useState } from "react";
+import { Route } from "react-router";
+import Catalog from "../../features/catalog/Catalog";
+import ProductDetails from "../../features/catalog/ProductDetails";
+import AboutPage from "../../features/about/AboutPage";
+import ContactPage from "../../features/contact/ContactPage";
 
 function App() {
     const [darkMode, setDarkMode] = useState(false);
@@ -29,7 +34,11 @@ function App() {
             <CssBaseline />
             <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
             <Container>
-                <Catalog />
+                <Route exact path='/' component={HomePage} />
+                <Route exact path='/catalog' component={Catalog} />
+                <Route path='/catalog/:id' component={ProductDetails} />
+                <Route path='/about' component={AboutPage} />
+                <Route path='/contact' component={ContactPage} />
             </Container>
         </ThemeProvider>
     );
