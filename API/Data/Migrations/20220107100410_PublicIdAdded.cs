6a7ea39eec7a2520a000f6cd48b865556e776650
marius.starke <marius.starke@flyeralarm-industrialprint.com>
2022-01-07 16:07:02 +0100

    InventoryComplete

---
+++ API/Data/Migrations/20220107100410_PublicIdAdded.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class PublicIdAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PublicId",
+                table: "Products",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "17b78799-a338-4389-99e6-556ca4c59177");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "4c6ed112-ee26-47de-8c66-86e898b7a04a");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PublicId",
+                table: "Products");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "bd16f69b-b595-44cc-a4fe-526544713a97");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "7a5526ad-5fb1-4c32-8bc2-bb2ece53c1a3");
+        }
+    }
+}
